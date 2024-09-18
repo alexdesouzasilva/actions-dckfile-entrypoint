03f3b0d41765fad16eb92c27ca308c449b680d9c
Alex <alexdesouzasilva94@gmail.com>
2024-09-18 19:42:59 -0300

    add sonnar scanner

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,17 @@ login=$4
 version=$5
 binaries=$6
 
+sudo apt-get update
+sudo apt-get install -y unzip
+wget -O sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
+unzip sonar-scanner.zip
+sudo mv sonar-scanner-4.6.2.2472-linux /opt/sonar-scanner
+sudo ln -s /opt/sonar-scanner/bin/sonar-scanner /usr/local/bin/sonar-scanner
+
 sonar-scanner \
-            -Dsonar.projectKey=$projectKey \
-            -Dsonar.sources=$sources \
-            -Dsonar.host.url=$url \
-            -Dsonar.login=$login \
-            -Dsonar.version=$version \
-            -Dsonar.java.binaries=$binaries
+    -Dsonar.projectKey=$projectKey \
+    -Dsonar.sources=$sources \
+    -Dsonar.host.url=$url \
+    -Dsonar.login=$login \
+    -Dsonar.version=$version \
+    -Dsonar.java.binaries=$binaries
